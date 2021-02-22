5556b392dcb987d05cce3f47cebf517cd20b5b1c
eriksteenberg <eriksteenberg1998@gmail.com>
2021-02-22 13:19:16 +0100

    Initial commit

--- exercise/exercise5/read-file.c
+++ exercise/exercise5/read-file.c
@@ -4,12 +4,14 @@
 int main(int argc, char** argv){
         int items;
         double x;
-        FILE* input_file = fopen(argv[1],"w");
+        FILE* input_file = fopen(argv[1],"r");
 	FILE* output_file = fopen(argv[2],"w");
 	do{
 		items = fscanf(input_file,"%lg",&x);
-                printf("x=%g: sin(x)=%g and cos(x)=%g\n",x,sin(x),cos(x));
+                fprintf(output_file,"x=%g: sin(x)=%g and cos(x)=%g\n",x,sin(x),cos(x));
 
 	}while(items!=EOF);
+fclose(input_file);
+fclose(output_file);
 return 0;
 }
