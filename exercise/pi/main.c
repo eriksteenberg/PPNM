ffb4b25a80f740c0ba75d3bdd27dcb0ba8ba12e8
eriksteenberg <eriksteenberg1998@gmail.com>
2021-03-03 14:05:56 +0100

    Hej

--- exercise/pi/main.c
+++ exercise/pi/main.c
@@ -1,28 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<gsl/gsl_rng.h>
 #include<pthread.h>
 #include<math.h>
-//#include<>
+#include<omp.h>
 
-struct parameters{int incirle, attempts; const gsl_rng * seed;};
+struct params {int N, Ninc; unsigned int seed;};
 
-void* my_pi(void* arg){
-	struct parameters * data = (struct paramters*)arg;
-	(*data).incirle=0;
-	double norm, xpoint, ypoint;
-	int N=(*data).attempts;
+void* my_pi(void* args){
+	struct params * data = (struct params*)args;
+	(*data).Ninc=0;
+	int N=(*data).N;
 	for(int i=0;i<N;i++){
-		xpoint = gsl_rng_uniform(data->seed);
-		ypoint = gsl_rng_uniform(data->seed);
-		norm = sqrt(pow(xpoint,2) + pow(ypoint,2));
-		if(norm<=1){
-			(*data).incirle++;
-		}
+		double xpoint = (double) rand_r(&(data->seed))/RAND_MAX;
+		double ypoint = (double) rand_r(&(data->seed))/RAND_MAX;
+		double norm = sqrt(pow(xpoint,2) + pow(ypoint,2));
+		if(norm<=1)(*data).Ninc++;
 	}
-	return NULL;
+return NULL;
 }
 
 int main(){
+	int Ntot = (int)1e8;
+	struct params n1 = {.N=Ntot/3, .Ninc=0, .seed = 5};
+	struct params n2 = {.N=Ntot/3, .Ninc=0, .seed = 7};
+	struct params n3 = {.N=Ntot/3, .Ninc=0, .seed = 15};
+	pthread_t p1,p2,p3;
+	pthread_create(&p1,NULL,my_pi,(void*)&n1);
+	pthread_create(&p2,NULL,my_pi,(void*)&n2);
+	pthread_create(&p3,NULL,my_pi,(void*)&n3);
+	pthread_join(p1,NULL);
+	pthread_join(p2,NULL);
+	pthread_join(p3,NULL);
+	int Ninc = n1.Ninc + n2.Ninc + n3.Ninc;
+	double pi = 4 * (double)Ninc/Ntot;
+//	printf("%i %i %10g\n",Ninc,Ntot,(double)Ninc/Ntot);
+	printf("my monte carlo simulation of pi gives %g. \nThe value of pi from math.h is %g \n",pi,M_PI);
 return 0;
 }
