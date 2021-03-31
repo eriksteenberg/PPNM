78b8c452bd4917c7b3b7192b110102b499961615
eriksteenberg <eriksteenberg1998@gmail.com>
2021-03-31 11:27:25 +0200

    hej

---
+++ homework/lineq/functions - Kopi.c
@@ -0,0 +1,79 @@
+#include<gsl/gsl_vector.h>
+#include<gsl/gsl_vector_double.h>
+#include<gsl/gsl_blas.h>
+#include<gsl/gsl_matrix.h>
+#include<math.h>
+#include<gsl/gsl_blas.h>
+#include<assert.h>
+#include<gsl/gsl_linalg.h>
+#include<stdio.h>
+#define rnd (double)rand()/RAND_MAX
+
+void show_matrix(gsl_matrix* A){
+	int n = A->size1; 
+	int m = A->size2; 
+	printf("\n");
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			double x = gsl_matrix_get(A,i,j);
+			printf("%9.3g ",x);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+double cdot(gsl_vector* A, gsl_vector* B){
+	assert(A->size==B->size);	
+	double result=0;
+	for(int i = 0;i<A->size;i++){
+		double b = gsl_vector_get(A,i)*gsl_vector_get(B,i);
+		result += b;	
+	}
+	return result;
+}
+
+
+void show_vector(gsl_vector* V){
+	int n = V->size;
+	printf("\n");
+	for(int i=0;i<n;i++){
+		printf("%g\n",gsl_vector_get(V,i));
+	}
+	printf("\n");
+}
+
+void GS_solve(gsl_matrix* Q, gsl_matrix* R, gsl_vector* b, gsl_vector* x){
+	int n = x->size;
+	gsl_blas_dgemv(CblasTrans,1,Q,b,0,x);
+	for(int i=n-1;i>0;i--){
+		double c1 = gsl_vector_get(x,i);
+		for(int j=i+1;j<n;j++){
+			c1 -= gsl_matrix_get(R,i,j)*gsl_vector_get(x,i);
+		}
+		gsl_vector_set(x,i,c1/gsl_matrix_get(R,i,i));
+	}
+}	
+
+
+void GS_decomp(gsl_matrix* A,gsl_matrix* R){
+	assert(A->size2 == R->size1);
+	int N = A->size1;
+	int M = A->size2;
+		gsl_vector* ai = gsl_vector_alloc(N);
+		gsl_vector* aj = gsl_vector_alloc(N);
+	for(int i=0;i<M;i++){
+                gsl_matrix_get_col(ai,A,i);
+		double Rii = gsl_blas_dnrm2(ai);
+		gsl_matrix_set(R,i,i,Rii);
+		gsl_vector_scale(ai,1/Rii);
+		gsl_matrix_set_col(A,i,ai);
+		for(int j=i+1;j<M;j++){
+	  		gsl_matrix_get_col(aj,A,j);
+			double Rij = cdot(ai,aj);
+			gsl_blas_daxpy(-Rij,ai,aj);
+			gsl_matrix_set(R,i,j,Rij);
+			gsl_matrix_set_col(A,j,aj);
+		}
+	}
+}
---
+++ homework/lineq/functions.h
@@ -0,0 +1,23 @@
+#include<gsl/gsl_vector.h>
+#include<gsl/gsl_vector_double.h>
+#include<gsl/gsl_blas.h>
+#include<gsl/gsl_matrix.h>
+#include<math.h>
+#include<assert.h>
+#include<gsl/gsl_linalg.h>
+#include<stdio.h>
+#define rnd (double)rand()/RAND_MAX
+void show_matrix(gsl_matrix* A);
+
+void show_vector(gsl_vector* V);
+
+double cdot(gsl_vector* A, gsl_vector* B);
+
+void GS_solve(gsl_matrix* Q, gsl_matrix* R, gsl_vector* b, gsl_vector* x);
+
+void GS_decomp(gsl_matrix* A,gsl_matrix* R);
+
+void matrix_multi(gsl_matrix* A, gsl_matrix* B,gsl_matrix* result);
+
+void transposed_multi(gsl_matrix* A, gsl_matrix* result);
+
